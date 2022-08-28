e8bb50ea2dee68718531e7c465ef8b6a692b71c3
Gabriel Attal <gab-attal@hotmail.fr>
2022-08-28 13:01:44 +0200

    refactor: update sentences

--- how-to-be-loved-in-game (League of Legends)/main.py
+++ how-to-be-loved-in-game (League of Legends)/main.py
@@ -8,8 +8,21 @@ import time
 from pynput.keyboard import Key, Controller
 
 screenWidth, screenHeight = pyautogui.size()
-i_killed = ["Thank you for the golds", "haha I'm better than you", "I love you", "how do you feel after I just destroyed you?"]
-i_died = ["report my team", "your charachter is for noobs.", "get a life", "ff15"]
+i_killed = ["/all Thank you for the golds", "/all haha I'm better than you",
+            "/all how do you feel after I just destroyed you?", "/all sit", "/all get rekt", "/all I feel your pain",
+            "/all EZ clapped", "/all I'm not arrogant, I'm good",
+            "/all Do you mind leaving the game? I would rather play with the AI.",
+            "/all Don't be coming up in my kitchen with your weaksauce.",
+            "/all Where's your homework? Cause you guys just got schooled.",
+            "/all Are you Neymar? Because I see you more often on the floor lying down than playing"]
+i_died = ["/all report my team",
+          "/all your champ is designed for people who have a social life, basically people who don't know how to play.",
+          "/all I'm done with this game", "/all jungler diff",
+          "/all I don't want to play this game, I just want to raise sheeps in the mountains",
+          "/all If I wanted to end with my life, I'd jump from your ego to your elo",
+          "/all If I wanted to support 4 talentless douchebags I could have just bought a Nickelback album",
+          "/all Its okay that you're all trash. It's called garbage can, not garbage cannot. Lets do this",
+          "/all You guys are garbanzo beans", "/all Your grandma makes cookies really really bad"]
 kills = 0
 deaths = 0
 
