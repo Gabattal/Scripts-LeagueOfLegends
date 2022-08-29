5dafc0f79c10423fff139c8b05d46e5d08d4a8fc
Gabriel Attal <gab-attal@hotmail.fr>
2022-08-29 02:19:55 +0200

    refactor : add sentences

--- how-to-be-loved-in-game (League of Legends)/main.py
+++ how-to-be-loved-in-game (League of Legends)/main.py
@@ -5,22 +5,36 @@ import requests
 import urllib3
 urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 
-i_killed = ["/all Thank you for the golds", "/all haha I'm better than you",
-            "/all how do you feel after I just destroyed you?", "/all sit", "/all get rekt", "/all I feel your pain",
+i_killed = ["/all How did this guy get passed the login screen ? ", "/all haha I'm better than you",
+            "/all how do you feel after I just destroyed you?", "/all sit", "/all get rekt",
             "/all EZ clapped", "/all I'm not arrogant, I'm good",
             "/all Do you mind leaving the game? I would rather play with the AI.",
             "/all Don't be coming up in my kitchen with your weaksauce.",
             "/all Where's your homework? Cause you guys just got schooled.",
-            "/all Are you Neymar? Because I see you more often on the floor lying down than playing"]
+            "/all Are you Neymar? Because I see you more often on the floor lying down than playing",
+            "/all I can't even call you an animal, these have a survival instinct at least",
+            "/all  I would call you a walking ward, but even wards live longer.",
+            "/all cry me a river and put a ward in it",
+            "/all *knock knock* -Who's there ? -Uni -Uni who? -Uninstall",
+            "/all  anyone has a good tutorial on how to change bots difficulty?",
+            "/all You are the single reason why Shurima is more dead than pre-rework Yorick",
+            "/all if you were a sandwich, you would be an idiot sandwich",
+            "/all if you cooked like you are playing, you would have a visit from Gordon Ramsay very soon..."
+            ]
 i_died = ["/all report my team",
-          "/all your champ is designed for people who have a social life, basically people who don't know how to play.",
+          "The scuttle crab applies more jungle pressure than you.  .",
           "/all I'm done with this game", "/all jungler diff",
           "/all I don't want to play this game, I just want to raise sheeps in the mountains",
           "/all If I wanted to end with my life, I'd jump from your ego to your elo",
           "/all If I wanted to support 4 talentless douchebags I could have just bought a Nickelback album",
-          "/all Its okay that you're all trash. It's called garbage can, not garbage cannot. Lets do this",
-          "/all You guys are garbanzo beans", "/all Your grandma makes cookies really really bad"]
-kills = 0
+          "Its okay that you're all trash. It's called garbage can, not garbage cannot. Lets do this",
+          "/all You guys are garbanzo beans", "/all Your grandma makes cookies really really bad",
+          "/all My team is so bad they can't even win a surrender vote",
+          "/all It's not my fault bot lane started with a Doran's Spoon and fed the shit out of Draven",
+          "/all Not even Noah can carry these animals",
+          "/all You would need a gank when laning against a minion",
+          "My push-up bra supports better than you"]
+
 deaths = 0
 
 
