717d1c44bbaff4307601c855088cab5075e82b71
Gaboubou <gab-attal@hotmail.fr>
2022-09-06 02:25:19 +0200

    fix: fix issues

--- Auto-Champ-Select/main.py
+++ Auto-Champ-Select/main.py
@@ -6,9 +6,25 @@ import webbrowser
 
 urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 
-in_game = False
-
 connector = Connector()
+global am_i_assigned, am_i_picking, am_i_banning, ban_number, phase, picks, bans, in_game
+am_i_assigned = False
+am_i_banning = False
+am_i_picking = False
+in_game = False
+phase = ''
+picks = []
+bans = []
+pick_number = 0
+ban_number = 0
+
+picks_file = open("picks.txt", "r").read().splitlines()
+for line in picks_file:
+    picks.append(line)
+
+bans_file = open("bans.txt", "r").read().splitlines()
+for line in bans_file:
+    bans.append(line)
 
 
 @connector.ready
@@ -34,61 +50,76 @@ async def ready_check_changed(connection, event):
 
 @connector.ws.register('/lol-champ-select/v1/session', event_types=('CREATE', 'UPDATE',))
 async def champ_select_changed(connection, event):
-    picks = []
-    bans = []
-    pick_number = 0
-    ban_number = 0
-    global in_game
+    global am_i_assigned, pick_number, ban_number, am_i_banning, am_i_picking, phase, bans, picks, have_i_prepicked, in_game, action_id
+    have_i_prepicked = False
+    lobby_phase = event.data['timer']['phase']
 
-    picks_file = open("picks.txt", "r").read().splitlines()
-    for line in picks_file:
-        picks.append(line)
+    local_player_cell_id = event.data['localPlayerCellId']
+    for teammate in event.data['myTeam']:
+        if teammate['cellId'] == local_player_cell_id:
+            assigned_position = teammate['assignedPosition']
+            am_i_assigned = True
 
-    bans_file = open("bans.txt", "r").read().splitlines()
-    for line in bans_file:
-        bans.append(line)
+    for action in event.data['actions']:
+        for actionArr in action:
+            if actionArr['actorCellId'] == local_player_cell_id and actionArr['isInProgress'] == True:
+                phase = actionArr['type']
+                action_id = actionArr['id']
+                if phase == 'ban':
+                    am_i_banning = actionArr['isInProgress']
+                if phase == 'pick':
+                    am_i_picking = actionArr['isInProgress']
 
-    phase = event.data['actions'][-1][0]['type']
-    action_id = event.data['actions'][-1][0]['id']
-    is_in_progress = event.data['actions'][-1][0]['isInProgress']
-
-    while phase == 'ban':
+    if phase == 'ban' and lobby_phase == 'BAN_PICK' and am_i_banning:
+        while am_i_banning:
+            try:
+                await connection.request('patch', '/lol-champ-select/v1/session/actions/%d' % action_id,
+                                         data={"championId": champions_map[bans[ban_number]], "completed": True})
+                ban_number += 1
+                am_i_banning = False
+            except (Exception,):
+                ban_number += 1
+                if ban_number > len(
+                        bans):  # Due to some lcu bugs I have to do this to correct a bug that may happen in draft custom
+                    ban_number = 0
+    if phase == 'pick' and lobby_phase == 'BAN_PICK' and am_i_picking:
+        while am_i_picking:
+            try:
+                await connection.request('patch', '/lol-champ-select/v1/session/actions/%d' % action_id,
+                                         data={"championId": champions_map[picks[pick_number]], "completed": True})
+                pick_number += 1
+                am_i_picking = False
+            except (Exception,):
+                pick_number += 1
+                if pick_number > len(
+                        picks):  # Due to some lcu bugs I have to do this to correct a bug that may happen in draft custom
+                    pick_number = 0
+    if lobby_phase == 'PLANNING' and not have_i_prepicked:
         try:
             await connection.request('patch', '/lol-champ-select/v1/session/actions/%d' % action_id,
-                                     data={"championId": champions_map[bans[ban_number]], "completed": False})
-            break
+                                     data={"championId": champions_map['Teemo'], "completed": False})
+            have_i_prepicked = True
         except (Exception,):
-            ban_number += 1
-    while phase == 'pick':
-        try:
-            await connection.request('patch', '/lol-champ-select/v1/session/actions/%d' % action_id,
-                                     data={"championId": champions_map[picks[pick_number]], "completed": True})
-            break
-        except (Exception,):
-            pick_number += 1
-
-    if not is_in_progress:
+            print(Exception)
+    if lobby_phase == 'FINALIZATION':
         while not in_game:
             try:
-                request_pseudo = requests.get('https://127.0.0.1:2999/liveclientdata/activeplayername', verify=False)
-                your_pseudo = request_pseudo.json()
-                response_API = requests.get(
-                    f'https://127.0.0.1:2999/liveclientdata/playerscores?summonerName={your_pseudo}',
-                    verify=False)
-                if request_pseudo and response_API and not in_game:
+                request_game_data = requests.get('https://127.0.0.1:2999/liveclientdata/allgamedata', verify=False)
+                game_data = request_game_data.json()['gameData']['gameTime']
+                if game_data > 0 and not in_game:
                     print("Game found!")
                     in_game = True
                     music_url = open("music.txt", "r").readline()
                     webbrowser.open(music_url, new=0, autoraise=True)
-                time.sleep(1)
+                time.sleep(2)
             except (Exception,):
                 print('Waiting for game to start...')
-                time.sleep(1)
+                time.sleep(2)
 
 
 @connector.close
 async def disconnect(_):
-    print('The client have been closed!')
+    print('The client has been closed!')
     await connector.stop()
 
 
